3e0ae80eea5ff82d543063d457c1feb70aa01f2b
Nuri1977 <nurilacka@gmail.com>
2022-03-17 17:42:28 +0100

    Add transition to cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import CartIcon from '../Cart/Carticon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../context/cart-context';
 
 function HeaderCartButton({ showCardHandler }) {
+  const [btnAnimated, setBtnAnimated] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberCartItems = cartCtx.items.reduce((curNumber, item) => curNumber + item.amount, 0);
+  const { items } = cartCtx;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnAnimated(true);
+
+    const timer = setTimeout(() => {
+      setBtnAnimated(false);
+    }, 300);
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  const numberCartItems = items.reduce((curNumber, item) => curNumber + item.amount, 0);
+
+  const btnClasses = `${classes.button} ${btnAnimated ? classes.bump : ''}`;
 
   return (
-    <button type="button" className={classes.button} onClick={showCardHandler}>
+    <button type="button" className={btnClasses} onClick={showCardHandler}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
